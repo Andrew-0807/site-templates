ed38210b3c7b6d38c69b47d3c5ff793212e7f407
Andrew-0807 <andreeeivlad0807@gmail.com>
2023-05-14 19:47:20 +0300

    DropDown

--- src/Task-Manager/tasks.js
+++ src/Task-Manager/tasks.js
@@ -8,6 +8,7 @@ import { useState } from 'react'
 import { taskList } from './Tasklist'
 import { categories } from './category'
 
+let selectedCategory = 'Select category'
 export const Tasks = () => {
     const [index, setIndex] = useState(0)
     const [value, setValue] = useState(false)
@@ -16,12 +17,31 @@ export const Tasks = () => {
     function nextTask() {
         if (index + 1 > taskList.length - 1) setIndex(0)
         else setIndex(index + 1)
+        task.category = selectedCategory
+        selectedCategory = 'Select category'
     }
     function categoryText() {
         return categories.map((name, index) => (
-            <li className="media" key={index}>
+            <motion.li
+                style={{
+                    display: 'inline-flex  ',
+                    flexDirection: 'column',
+                }}
+                initial={{ scale: 0.9, x: 1, y: 10 }}
+                animate={{ rotate: 0, scale: 1, x: 0, y: 0 }}
+                transition={{
+                    x: '0%',
+                    delay: 0.1,
+                    opacity: 0,
+                    transition: { duration: 1 },
+                    transitionEnd: { display: 'none' },
+                }}
+                className="media"
+                key={index}
+                id="selectable"
+                onClick={() => handleCategorySelect(name)}>
                 {name}
-            </li>
+            </motion.li>
         ))
     }
 
@@ -29,32 +49,39 @@ export const Tasks = () => {
         if (value) categoryText()
         else setValue(!value)
     }
-
+    function handleCategorySelect(namea) {
+        selectedCategory = namea
+        task.category = selectedCategory
+        console.log(selectedCategory)
+        setValue(!value)
+    }
+    function hasCategory() {
+        if (task.hasOwnProperty('category')) {
+            return task.category
+        }
+        return selectedCategory
+    }
     function ShowTaskList() {
         return (
-            <div id="Tasks">
-                <div className=" ">
-                    <div className="">
-                        <div className="">
-                            <motion.div></motion.div>
-                            <h6
-                                initial={{ x: -100, y: +20 }}
-                                animate={{ x: 0, y: 0 }}>
-                                {task.Title}
-                            </h6>
-                            <p onClick={showCategories}>
-                                {value ? categoryText() : 'Show'}
-                            </p>
-                            <motion.button
-                                id="btn-next"
-                                whileTap={{ scale: 1.3, color: '#a6e3a1' }}
-                                onClick={nextTask}>
-                                Next
-                            </motion.button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <motion.div id="Tasks">
+                <h6 initial={{ x: -100, y: +20 }} animate={{ x: 0, y: 0 }}>
+                    {task.Title}
+                </h6>
+
+                <p
+                    style={value ? { background: '#1e1e2e' } : {}}
+                    id="dropDown"
+                    onClick={showCategories}>
+                    {value ? categoryText() : hasCategory()}
+                </p>
+
+                <motion.button
+                    id="btn-next"
+                    whileTap={{ scale: 1.3, color: '#a6e3a1' }}
+                    onClick={nextTask}>
+                    Next
+                </motion.button>
+            </motion.div>
         )
     }
     return (

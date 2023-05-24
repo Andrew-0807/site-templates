7261c21616e6398031999adea414ddfd2fc0ddce
Andrew-0807 <andreeeivlad0807@gmail.com>
2023-05-24 12:55:23 +0300

    #

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './navbar.scss'
 
-import { useState } from 'react'
-
 function Navbar() {
-    const [isShown, setIsShown] = useState(false)
-
     return <div className=""></div>
 }
 
